7b727e5d0d7879e031f85699baa5547a831fed67
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2022-02-26 12:19:44 +0530

    assignment 1 document added

--- Test/test.c
+++ Test/test.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-    int input, flag = 0;
-    printf("Enter a number: ");
-    scanf("%d", &input);
-
-    for (int i = 2; i <= input/2; i++)
-    {
-        if (input%i == 0)
-        {
-            flag = 1;
-        }
-    }
-    if (flag == 1)
-    {
-        printf("Not Prime");
-    }
-    else
-    {
-        printf("Prime");
-    }
+    int a = 33, b = 47, c;
+    c = a & b;
+    printf("\na & b = %d", c);
+    c = c >> 4;
+    printf("\nc >> 4 = %d", c);
+    printf("\n");
     return 0;
 }
+
+
+// void main() {
+//     int a[] = {3,1,5,4}, i, j, s = 0;
+//     for (i = 0; i <=4; i++) s+=a[i];
+//     for(i = 1; i<= s; i++) {
+//         for(j = 0; j<4; j++)
+//             if (i==a[j])
+//             {
+//                 printf("%d", i);
+//             }
+            
+//     }
+// }
+
+// int main()
+// {
+//     int input, flag = 0;
+//     printf("Enter a number: ");
+//     scanf("%d", &input);
+
+//     for (int i = 2; i <= input/2; i++)
+//     {
+//         if (input%i == 0)
+//         {
+//             flag = 1;
+//         }
+//     }
+//     if (flag == 1)
+//     {
+//         printf("Not Prime");
+//     }
+//     else
+//     {
+//         printf("Prime");
+//     }
+//     return 0;
+// }
 
 // int main() {
 //     int last = 0, current = 1, next, n;
